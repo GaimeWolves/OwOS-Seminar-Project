835de678baf289634074346049192502e7f6aabc
Nick Djerfi <nick@acer1705.localhost>
2020-03-04 14:25:07 +0100

    Wrote setupiso target for OS Makefile
    	- Added stub.c to be used in stub OS Image if compilation of OS
    	  fails
    	- Wrote setupiso target to copy OS Image to ISO

---
+++ src/os/stub.c
@@ -0,0 +1,15 @@
+void write_string(char colour, const char *string)
+{
+    volatile char *video = (volatile char*)0xB8000;
+    while(*string != 0)
+    {
+        *video++ = *string++;
+        *video++ = colour;
+    }
+}
+
+void entry(void* boot_info)
+{
+	write_string(0x0F, "Something went wrong in compilation, this is just a stub to prevent the bootloader from crashing.");
+	for(;;);
+}
