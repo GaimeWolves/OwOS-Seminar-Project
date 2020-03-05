639fa548cae3d1ce5e87d4c203596708930129a6
Philipp Rempe <philipp.rempe@gmx.net>
2020-03-05 16:59:21 +0100

    Declare malloc etc.
    They will later be implemented by the memory driver

--- src/os/include/stdlib.h
+++ src/os/include/stdlib.h
@@ -3,3 +3,8 @@
 #include <size_t.h>
 char* uitoa(size_t num, char* buf, size_t base, bool prepend_zeros);
 char* itoa(int num, char* buf, size_t base, bool prepend_zeros);
+void* malloc(size_t size);
+void free(void *ptr);
+void* calloc(size_t nmemb, size_t size);
+void* realloc(void *ptr, size_t size);
+void* reallocarray(void *ptr, size_t nmemb, size_t size);
