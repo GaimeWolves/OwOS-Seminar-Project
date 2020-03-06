da23aef9c9749978a9541d29098567a0839818a2
Robin Ebert <ebert-robin@web.de>
2020-03-06 10:40:25 +0100

    Adjusted Format renormalized end-of-lines use tabs instead of 4 spaces

--- src/os/stub.c
+++ src/os/stub.c
@@ -1,11 +1,11 @@
 void write_string(char colour, const char *string)
 {
-    volatile char *video = (volatile char*)0xB8000;
-    while(*string != 0)
-    {
-        *video++ = *string++;
-        *video++ = colour;
-    }
+	volatile char *video = (volatile char*)0xB8000;
+	while(*string != 0)
+	{
+		*video++ = *string++;
+		*video++ = colour;
+	}
 }
 
 void entry(void* boot_info)
