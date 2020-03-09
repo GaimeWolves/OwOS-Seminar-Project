d7c1bcffe5aa36db54bb08561f556fe601890b35
Robin Ebert <ebert-robin@web.de>
2020-03-09 15:36:09 +0100

    Added basic Stage2 main.asm file structure

--- src/bootloader/Stage2/main.asm
+++ src/bootloader/Stage2/main.asm
@@ -1,4 +1,47 @@
 [bits 16]
 [org 0x9000]
-
+;-----------------------------------------
+;	Main
+;	Called with
+;	CS = 0
+;	DL = drive number				=> Kernel
+;	DS:SI = partition table entry	=> Kernel
+;-----------------------------------------
+cli
 hlt
+
+;-----------------------------------------
+;	Constants
+;-----------------------------------------
+STACK_POSITION					equ 0xFFFF
+KERNEL_LOAD_SEGMENT				equ 0x1000
+KERNEL_LOAD_OFFSET				equ 0x0000
+KERNEL_ADDRESS					equ 0x100000
+
+;-----------------------------------------
+;	Variables
+;-----------------------------------------
+FileName:						db "KERNEL  SYS"
+
+;-----------------------------------------
+;	Includes
+;-----------------------------------------
+%include "../Help_Functions/struct.inc"
+%include "../Help_Functions/memory_lba.inc"
+%include "../Help_Functions/FAT32_FAT.inc"
+%include "../Help_Functions/FAT32_RootDir.inc"
+%include "../Help_Functions/MBR.inc"
+%include "../Help_Functions/FAT32_struct.inc"
+
+;-----------------------------------------
+[bits 32]
+;-----------------------------------------
+;	Main32
+;	Called with
+;	CS = 0
+;	DL = drive number				=> Kernel
+;	DS:SI = partition table entry	=> Kernel
+;-----------------------------------------
+main32:
+	cli
+	hlt
