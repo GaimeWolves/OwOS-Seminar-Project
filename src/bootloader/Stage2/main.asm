ec8bc669521deccd347b0cca922919962bc54663
Robin Ebert <ebert-robin@web.de>
2020-03-09 15:59:16 +0100

    Stage2 load kernel code added

--- src/bootloader/Stage2/main.asm
+++ src/bootloader/Stage2/main.asm
@@ -1,5 +1,13 @@
 [bits 16]
 [org 0x9000]
+
+;-----------------------------------------
+;	Includes (Preprocessor)
+;-----------------------------------------
+%include "../Help_Functions/struct.inc"
+%include "../Help_Functions/MBR.inc"
+%include "../Help_Functions/FAT32_struct.inc"
+
 ;-----------------------------------------
 ;	Main
 ;	Called with
@@ -7,6 +15,49 @@
 ;	DL = drive number				=> Kernel
 ;	DS:SI = partition table entry	=> Kernel
 ;-----------------------------------------
+;We don't want to be interrupted currently
+cli
+
+;Set stack related registers
+push 0
+pop ss					;Stack segment = 0
+mov sp, STACK_POSITION	;Stack pointer = STACK_POSITION
+
+;Save the variables passed by the Stage1 bootstrap code
+push si ;Pass to Kernel
+push ds ;Pass to Kernel
+push dx ;Pass to Kernel
+push dx ;Restore when loading the cluster chain
+
+;Get partition's LBA offset
+GetMemberDWord(ebx,si,PartitionEntry.LBA) ;LBA offset is saved in the partition entry struct in ds:si 
+push ebx ;Restore when loading the cluster chain
+
+;set data segment register to 0
+push 0
+pop ds
+;code segment register already contains 0
+
+;Now interrupt are safe
+sti
+
+;Search kernel file in the root dir
+mov di, FileName ;"KERNEL  SYS"
+call SearchFile
+
+;load to 0x0:STAGE2_OFFSET
+push KERNEL_LOAD_SEGMENT
+pop ds					;Segment KERNEL_LOAD_SEGMENT
+mov si, KERNEL_LOAD_OFFSET	;Offset KERNEL_LOAD_OFFSET
+;Load the sectors of the clusterchain
+pop ebx	;Restore partition's LBA offset
+pop dx	;Restore drive number
+
+;Load cluster chain
+call LoadClusterChain
+
+
+
 cli
 hlt
 
@@ -22,16 +73,17 @@ KERNEL_ADDRESS					equ 0x100000
 ;	Variables
 ;-----------------------------------------
 FileName:						db "KERNEL  SYS"
+KernelSectorCount:				dw 0
+%include "../Help_Functions/memory_lba_var.inc"
+%include "../Help_Functions/FAT32_FAT_var.inc"
+%include "../Help_Functions/FAT32_RootDir_var.inc"
 
 ;-----------------------------------------
 ;	Includes
 ;-----------------------------------------
-%include "../Help_Functions/struct.inc"
 %include "../Help_Functions/memory_lba.inc"
 %include "../Help_Functions/FAT32_FAT.inc"
 %include "../Help_Functions/FAT32_RootDir.inc"
-%include "../Help_Functions/MBR.inc"
-%include "../Help_Functions/FAT32_struct.inc"
 
 ;-----------------------------------------
 [bits 32]
