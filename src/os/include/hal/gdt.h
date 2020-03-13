88995b5442974acaf7d9b366d0a053067622661c
Robin Ebert <ebertrobin2002@gmail.com>
2020-03-13 16:32:28 +0100

    Added first hal headers

---
+++ src/os/include/hal/gdt.h
@@ -0,0 +1,58 @@
+#ifndef _GDT_H
+#define _GDT_H
+//------------------------------------------------------------------------------------------
+//				Includes
+//------------------------------------------------------------------------------------------
+#include<stdint.h>
+
+//------------------------------------------------------------------------------------------
+//				Constants
+//------------------------------------------------------------------------------------------
+#define CODE_DESCRIPTOR_RING_0 0x08
+#define DATA_DESCRIPTOR_RING_0 0x10
+#define GDT_MAX_COUNT		   0x03
+
+#define GDT_DESC_ACCESS_ACCESS		0b00000001 //Don't set
+#define GDT_DESC_ACCESS_READWRITE	0b00000010
+#define GDT_DESC_ACCESS_EXPANSION	0b00000100
+#define GDT_DESC_ACCESS_EXEC_CODE	0b00001000
+#define GDT_DESC_ACCESS_CODEDATA	0b00010000
+#define GDT_DESC_ACCESS_DPL_RING0	0b00000000
+#define GDT_DESC_ACCESS_DPL_RING1	0b00100000
+#define GDT_DESC_ACCESS_DPL_RING2	0b01000000
+#define GDT_DESC_ACCESS_DPL_RING3	0b01100000
+#define GDT_DESC_ACCESS_MEMORY		0b10000000
+
+#define GDT_DESC_FLAGS_32BIT		0b0100
+#define GDT_DESC_FLAGS_GRANULARITY	0b1000
+
+//------------------------------------------------------------------------------------------
+//				Types
+//------------------------------------------------------------------------------------------
+typedef struct
+{
+	uint16_t limitLow;
+	uint16_t baseLow;
+	uint8_t baseMiddle;
+	uint8_t access;
+	uint8_t limitHigh_granularity;
+	uint8_t baseHigh;
+} __attribute__((packed)) gdtDescriptor_t;
+
+typedef struct
+{
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed)) gdtr_t;
+
+//------------------------------------------------------------------------------------------
+//				Variables
+//------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------
+//				Public Function
+//------------------------------------------------------------------------------------------
+int initGDT(void);
+
+
+#endif
---
+++ src/os/include/hal/interrupt.h
@@ -0,0 +1,76 @@
+#ifndef _INTERRUPT_H
+#define _INTERRUPT_H
+//------------------------------------------------------------------------------------------
+//				Includes
+//------------------------------------------------------------------------------------------
+#include <stdint.h>
+
+//------------------------------------------------------------------------------------------
+//				Constants
+//------------------------------------------------------------------------------------------
+#define _32_TASK_GATE       0b00000101
+#define _16_INTERRUPT_GATE  0b00000110
+#define _16_TRAP_GATE       0b00000111
+#define _32_INTERRUPT_GATE  0b00001110
+#define _32_TRAP_GATE       0b00001111
+
+#define _STORAGE_SEGMENT    0b00010000
+
+#define _RING_3_DPL         0b01100000
+#define _RING_2_DPL         0b01000000
+#define _RING_1_DPL         0b00100000
+#define _RING_0_DPL         0b00000000
+
+#define _USED               0b10000000
+#define _UNUSED             0b00000000
+
+#define I86_MAX_INTERRUPTS  256
+
+//------------------------------------------------------------------------------------------
+//				Types
+//------------------------------------------------------------------------------------------
+typedef struct
+{
+    //Length of IDT - 1
+    uint16_t limit;
+
+    //Start of the IDT
+    uint32_t base;
+} __attribute__((packed)) IDTR_t;
+
+typedef struct
+{
+    uint16_t    offset_1;   //offset bit 0..15
+    uint16_t    descriptor; //a code descriptor in GDT or LDT
+    uint8_t     zero;       //set to 0
+    uint8_t     type_attr;  //type and attribute
+    uint16_t    offset_2;   //offset bit 16..31
+} __attribute__((packed)) IDTDescriptor_t;
+
+typedef struct
+{
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+} __attribute__((packed)) InterruptFrame_t;
+
+
+typedef void (__attribute__((__interrupt__)) *interruptHandler_t)(InterruptFrame_t*);
+
+//------------------------------------------------------------------------------------------
+//				Variables
+//------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------
+//				Public Function
+//------------------------------------------------------------------------------------------
+int initIDT(void);
+
+int setVect(uint8_t, interruptHandler_t);
+void (__attribute__((__interrupt__)) *getVect(uint8_t))(InterruptFrame_t*);
+
+int setInterruptFlag(void);
+int resetInterruptFlag(void);
+
+void genInt(uint8_t id);
+#endif _INTERRUPT_H
