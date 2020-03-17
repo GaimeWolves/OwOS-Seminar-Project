68f1dd3186314b206ab6a73e9d334d20086f3486
Nick Djerfi <gamewolvesofficial@gmail.com>
2020-03-17 15:15:17 +0100

    Fixed some bugs in Makefile and stub
    	- GCC library should be linked against last
    	- Wildcard for stub should only execute if kernel fails
    	- Added _Noreturn for stub

--- src/os/stub.c
+++ src/os/stub.c
@@ -1,16 +1,18 @@
+#include <stdnoreturn.h>
+
 #include <stdint.h>
 
 void write_string(char colour, const char *string)
 {
-    volatile char *video = (volatile char*)0xB8000;
-    while(*string != 0)
-    {
-        *video++ = *string++;
-        *video++ = colour;
-    }
+	volatile char *video = (volatile char*)0xB8000;
+	while(*string != 0)
+	{
+		*video++ = *string++;
+		*video++ = colour;
+	}
 }
 
-void main(uint32_t magic, void* boot_info)
+_Noreturn void main(uint32_t magic, void* boot_info)
 {
 	write_string(0x0F, "Something went wrong in compilation, this is just a stub to prevent the bootloader from crashing.");
 	for(;;);
