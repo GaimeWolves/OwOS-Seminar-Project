c6c0069f646a4838859f0458b73b2271dbe1bcb4
Molytho <>
2020-05-03 18:13:37 +0200

    Added in functions

--- src/os/hal/cpu.c
+++ src/os/hal/cpu.c
@@ -34,3 +34,23 @@ void outd(uint16_t port, uint32_t data)
 {
 	__asm__("out dx, eax"::"a"(data),"d"(port));
 }
+
+uint8_t inb(uint16_t port)
+{
+	uint8_t data = 0;
+	__asm__("in al, dx":"=a"(data):"d"(port));
+	return data;
+}
+uint16_t inw(uint16_t port)
+{
+	uint16_t data = 0;
+	__asm__("in ax, dx":"=a"(data):"d"(port));
+	return data;
+}
+
+uint32_t ind(uint16_t port)
+{
+	uint32_t data = 0;
+	__asm__("in eax, dx":"=a"(data):"d"(port));
+	return data;
+}
--- src/os/include/hal/cpu.h
+++ src/os/include/hal/cpu.h
@@ -26,4 +26,9 @@ char* vendorName(void);
 void outb(uint16_t port, uint8_t data);
 void outw(uint16_t port, uint16_t data);
 void outd(uint16_t port, uint32_t data);
+
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+uint32_t ind(uint16_t port);
+
 #endif
