4a326e864b2d60aaa211f3fc9ea9ea2d4d2f8c20
Philipp Rempe <philipp.rempe@gmx.net>
2020-05-06 18:25:11 +0200

    Bitmap library

---
+++ src/os/kernel/keyboard/bitmap.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#include <stdbool.h>
+static inline void setBit(uint32_t bit, uint32_t bitmap[]);
+static inline void clearBit(uint32_t bit, uint32_t bitmap[]);
+static inline bool getBit(uint32_t bit, uint32_t bitmap[]);
