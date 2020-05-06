0989da6e8b93e602d4166e652ca99c77e73737f8
Philipp Rempe <philipp.rempe@gmx.net>
2020-05-06 16:09:15 +0200

    Keyboard functions and definitions

---
+++ src/os/hal/keyboard.c
@@ -0,0 +1,54 @@
+#include <hal/keyboard.h>
+#include <hal/cpu.h>
+#include <stdbool.h>
+
+// Read from keyboard controller status register
+uint8_t kbCtrlReadStatus () {
+	return inb(KB_CTRL_STATS_REG);
+}
+
+// Returns true if keyboard controller buffer is full
+bool kbCtrlBufFull() {
+	return kbCtrlReadStatus() & KB_CTRL_STATS_MASK_IN_BUF;
+}
+
+// Wait for keyboard controller buffer to be empty
+void kbCtrlWaitEmpty() {
+	while (kbCtrlBufFull());
+}
+
+// Wait for keyboard controller buffer to be full
+void kbCtrlWaitFull() {
+	while (kbCtrlBufFull());
+}
+
+// Send command to keyboard controller
+void kbCtrlSendCmd (uint8_t cmd) {
+	kbCtrlWaitEmpty(); 
+	outb(KB_CTRL_CMD_REG, cmd);
+}
+
+// Enable keyboard
+void kbEnable () {
+	kbCtrlSendCmd(KB_CTRL_CMD_ENABLE);
+}
+
+// Disable keyboard
+void kbDisable () {
+	kbCtrlSendCmd(KB_CTRL_CMD_DISABLE);
+}
+
+// Read keyboard encoder input buffer
+uint8_t kbEncReadBuf() {
+	return inb(KB_ENC_INPUT_BUF);
+}
+
+// Run keyboard self-test
+bool kbSelfTest() {
+	kbCtrlSendCmd(KB_CTRL_CMD_SELF_TEST);
+
+	kbCtrlWaitFull();
+
+	// Test passed if output buffer is 0x55
+	return (kbEncReadBuf() == 0x55) ? true : false;
+}
---
+++ src/os/include/hal/keyboard.h
@@ -0,0 +1,58 @@
+#define KB_CTRL_STATS_MASK_OUT_BUF      0b00000001
+#define KB_CTRL_STATS_MASK_IN_BUF       0b00000010
+#define KB_CTRL_STATS_MASK_SYSTEM       0b00000100
+#define KB_CTRL_STATS_MASK_CMD_DATA     0b00001000
+#define KB_CTRL_STATS_MASK_LOCKED       0b00010000
+#define KB_CTRL_STATS_MASK_AUX_BUF      0b00100000
+#define KB_CTRL_STATS_MASK_TIMEOUT      0b01000000
+#define KB_CTRL_STATS_MASK_PARITY       0b10000000
+#define KB_ENC_INPUT_BUF                0x60
+#define KB_ENC_CMD_REG                  0x60
+
+#define KB_CTRL_STATS_REG               0x64
+#define KB_CTRL_CMD_REG                 0x64
+
+#define KB_ENC_CMD_SET_LED              0xED
+#define KB_ENC_CMD_ECHO                 0xEE
+#define KB_ENC_CMD_SCAN_CODE_SET        0xF0
+#define KB_ENC_CMD_ID                   0xF2
+#define KB_ENC_CMD_AUTODELAY            0xF3
+#define KB_ENC_CMD_ENABLE               0xF4
+#define KB_ENC_CMD_RESETWAI             0xF5
+#define KB_ENC_CMD_RESETSCAN            0xF6
+#define KB_ENC_CMD_ALL_AUTO             0xF7
+#define KB_ENC_CMD_ALL_MAKEBREAK        0xF8
+#define KB_ENC_CMD_ALL_MAKEONLY         0xF9
+#define KB_ENC_CMD_ALL_MAKEBREAK_AUTO   0xFA
+#define KB_ENC_CMD_SINGLE_AUTOREPEAT    0xFB
+#define KB_ENC_CMD_SINGLE_MAKEBREAK     0xFC
+#define KB_ENC_CMD_SINGLE_BREAKONLY     0xFD
+#define KB_ENC_CMD_RESEND               0xFE
+#define KB_ENC_CMD_RESET                0xFF
+
+#define KB_CTRL_CMD_READ                0x20
+#define KB_CTRL_CMD_WRITE               0x60
+#define KB_CTRL_CMD_SELF_TEST           0xAA
+#define KB_CTRL_CMD_INTERFACE_TEST      0xAB
+#define KB_CTRL_CMD_DISABLE             0xAD
+#define KB_CTRL_CMD_ENABLE              0xAE
+#define KB_CTRL_CMD_READ_IN_PORT        0xC0
+#define KB_CTRL_CMD_READ_OUT_PORT       0xD0
+#define KB_CTRL_CMD_WRITE_OUT_PORT      0xD1
+#define KB_CTRL_CMD_READ_TEST_INPUTS    0xE0
+#define KB_CTRL_CMD_SYSTEM_RESET        0xFE
+#define KB_CTRL_CMD_MOUSE_DISABLE       0xA7
+#define KB_CTRL_CMD_MOUSE_ENABLE        0xA8
+#define KB_CTRL_CMD_MOUSE_PORT_TEST     0xA9
+#define KB_CTRL_CMD_MOUSE_WRITE         0xD4
+
+#include <stdint.h>
+#include <stdbool.h>
+uint8_t kbCtrlReadStatus();
+bool kbCtrlBufFull();
+void kbCtrlWaitEmpty();
+void kbCtrlSendCmd (uint8_t cmd);
+void kbEnable();
+void kbDisable();
+uint8_t kbEncReadBuf();
+bool kbSelfTest();
