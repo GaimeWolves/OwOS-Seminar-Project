b0c0ea5f26eea82035b2904ca647843e222367b9
Molytho <>
2020-06-05 19:26:41 +0200

    Start shell in main method

--- src/os/kernel/main.c
+++ src/os/kernel/main.c
@@ -5,6 +5,7 @@
 #include <memory/pmm.h>
 #include <keyboard.h>
 #include <vfs/vfs.h>
+#include <shell/shell.h>
 
 _Noreturn void main(uint32_t magic, multiboot_info_t *boot_info)
 {
@@ -13,6 +14,12 @@ _Noreturn void main(uint32_t magic, multiboot_info_t *boot_info)
 	initKeyboard();
 	initVFS();
 	setInterruptFlag();
+
+	//Initialize shell
+	shell_init();
+	//Start shell loop
+	//We will not return
+	shell_start();
 	
 	// The kernel should never return
 	for (;;);
