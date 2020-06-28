3dc891e8b64d6432cdd91aaa6f8e294b3f0050e7
Philipp Rempe <philipp.rempe@gmx.net>
2020-06-28 13:17:38 +0200

    Read and display file

--- src/os/applications/editor/main.c
+++ src/os/applications/editor/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../../include/shell/in_stream.h"
 #include "../../include/display/cells.h"
 #include "../../include/keyboard.h"
 #include "../../include/vfs/vfs.h"
 
 FILE* in_stream;
+FILE* file;
+char* buffer;
 
 enum Mode
 {
@@ -17,6 +21,12 @@ enum Mode
 enum Mode mode;
 
 void refreshScreen() {
+	for (int y = 0; y < 25; y++) {
+		for (int x = 0; x < 80; x++) {
+			char c = buffer[y*80 + x];
+			addchr(x, y, c ? c : ' ');
+		}
+	}
 	refresh();
 }
 
@@ -39,8 +49,34 @@ void handleKeypress() {
 	}
 }
 
+int readFile(char* filename) {
+	FILE* file = fopen(filename, "r");
+	buffer = malloc(2001);
+	memset(buffer, 0, 2001);
+	size_t read;
+	while(!feof(file)) {
+		if((read = fread(buffer, sizeof(char), 2000, file))) {
+			if(read != 2000)
+			{
+				if(ferror(file))
+				{
+					//Print error with file name
+					perror(file->file_desc->name);
+					return -1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
+	if (readFile(argv[1]) != 0) {
+		return -1;
+	}
+	clrscr();
+	refresh();
 	mode = Normal;
 	set_color(FOREGROUND_WHITE, BACKGROUND_BLACK);
 	in_stream = shell_in_stream_get();
