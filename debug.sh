df3b9d9f33df361975e576ff41ff9036a738b20d
Nick Djerfi <gamewolvesofficial@gmail.com>
2023-04-13 13:54:48 +0200

    remove comment

--- debug.sh
+++ debug.sh
@@ -1,3 +1,1 @@
-# gdb -ex "file build/os/kernel.elf" -ex "set architecture i386:x86-64:intel" -ex "target remote localhost:1234"
-
 qemu-system-i386 -s -S -no-reboot -no-shutdown -chardev stdio,id=char0,mux=on,logfile=log.txt,signal=off -serial chardev:char0 -hda build/disk.iso -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 & sleep 1 && gdb -ex "target remote localhost:1234" -ex "symbol-file build/os/kernel.elf"
